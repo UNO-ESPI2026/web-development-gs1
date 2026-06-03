f9c3f6fe72051fee975c4c397ac5328b0f81bd94
Giovanna <gparejas23@outlook.com>
2026-06-03 16:04:44 -0300

    feat: Implementando funcionalidade JS no slideshow

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,23 @@ hamburguer.addEventListener('click', () => {
 });
 
 // Fim: Menu
+
+// Início: Slideshow 
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function () {
+    nextImage();
+}, 2500)
+
+function nextImage() {
+    count++;
+    if (count > 3) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
+// Fim: Slideshow
